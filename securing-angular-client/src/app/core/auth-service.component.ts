4efb8e26da0fd7ba6adc19be494c07871cf902d6
vini-politta <vinicius.politta3@gmail.com>
2020-04-27 16:42:36 -0300

    new repositorio auth angular

---
+++ securing-angular-client/src/app/core/auth-service.component.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { UserManager, User } from 'oidc-client';
+import { Constants } from '../constants';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class AuthService {
+  private _userManager: UserManager;
+  private _user: User;
+  private _loginChangedSubject = new Subject<boolean>();
+
+  loginChanged = this._loginChangedSubject.asObservable();
+
+  constructor() {
+    const stsSettings = {
+      authority: Constants.stsAuthority,
+      client_id: Constants.clientId,
+      redirect_uri: `${Constants.clientRoot}signin-callback`,
+      scope: 'openid profile projects-api',
+      response_type: 'code',
+      post_logout_redirect_uri: `${Constants.clientRoot}signout-callback`,
+      // metadata: {
+      //   issuer: `${Constants.stsAuthority}`,
+      //   authorization_endpoint: `${Constants.stsAuthority}authorize?audience=projects-api`,
+      //   jwks_uri: `${Constants.stsAuthority}.well-known/jwks.json`,
+      //   token_endpoint: `${Constants.stsAuthority}oauth/token`,
+      //   userinfo_endpoint: `${Constants.stsAuthority}userinfo`,
+      //   end_session_endpoint: `${Constants.stsAuthority}v2/logout?client_id=${Constants.clientId}&returnTo=${encodeURI(Constants.clientRoot)}signout-callback`
+      // }
+    };
+    this._userManager = new UserManager(stsSettings);
+  }
+
+  login() {
+    return this._userManager.signinRedirect();
+  }
+
+  isLoggedIn(): Promise<boolean> {
+    return this._userManager.getUser().then(user => {
+      const userCurrent = !!user && !user.expired;
+      if (this._user !== user) {
+        this._loginChangedSubject.next(userCurrent);
+      }
+      this._user = user;
+      return userCurrent;
+    });
+  }
+
+  completeLogin() {
+    return this._userManager.signinRedirectCallback().then(user => {
+      this._user = user;
+      this._loginChangedSubject.next(!!user && !user.expired);
+      return user;
+    });
+  }
+
+  logout() {
+    this._userManager.signoutRedirect();
+  }
+
+  completeLogout() {
+    this._user = null;
+    return this._userManager.signoutRedirectCallback();
+  }
+
+  getAccessToken() {
+    return this._userManager.getUser().then(user => {
+      if (!!user && !user.expired) {
+        return user.access_token;
+      }
+      else {
+        return null;
+      }
+    });
+  }
+}
