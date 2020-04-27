b1685e2e5532df5626aaec4c0a7636a960c5c5d5
vini-politta <vinicius.politta3@gmail.com>
2020-04-27 17:00:31 -0300

    change endpoit

--- securing-angular-client/src/app/constants.ts
+++ securing-angular-client/src/app/constants.ts
@@ -2,11 +2,11 @@
 export class Constants {
 	public static clientRoot = 'http://localhost:4200/';
 
-	public static apiRoot = 'https://securingangularappscoursev2-api.azurewebsites.net/api/';
-	public static stsAuthority = 'https://securingangularappscoursev2-sts.azurewebsites.net/';
+	// public static apiRoot = 'https://securingangularappscoursev2-api.azurewebsites.net/api/';
+	// public static stsAuthority = 'https://securingangularappscoursev2-sts.azurewebsites.net/';
 
-	// public static apiRoot = 'http://localhost:2112/api/';
-	// public static stsAuthority = 'http://localhost:4242/';
+	public static apiRoot = 'http://localhost:2112/api/';
+	public static stsAuthority = 'http://localhost:4242/';
 	// public static stsAuthority = 'https://briannoyes.auth0.com/';
 
 	public static clientId = 'spa-client';
